2cfa20ce3c0fcc7d5a61404dd596a7d371626f52
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2025-02-03 17:02:10 +0100

    trigger buid

--- src/Sample-CSharp/VulnerableController.cs
+++ src/Sample-CSharp/VulnerableController.cs
@@ -3,20 +3,4 @@ using System.Xml;
 
 public class ExampleController : Controller
 {
-
-    public void x()
-    {
-
-    }
-
-
-    [HttpGet]
-    public IActionResult Authenticate(string user, string pass)
-    {
-        var doc = new XmlDocument();
-
-        var expression = "/users/user[@name='" + user + "' and @pass='" + pass + "']";
-
-        return Json(doc.SelectSingleNode(expression) != null);
-    }
 }
