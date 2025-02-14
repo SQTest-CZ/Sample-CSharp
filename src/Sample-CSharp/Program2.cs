2664115c841666795efbe0c3a6c722acfcf90a55
Pawel Kupinski <pawel.kupinski@sonarsource.com>
2025-02-14 16:45:20 +0100

    new commit

---
+++ src/Sample-CSharp/Program2.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// todo: raise issue here
+
+static void WriteToDirectory(ZipArchiveEntry entry, string destDirectory)
+{
+    string destFileName = Path.Combine(destDirectory, entry.FullName);
+    entry.ExtractToFile(destFileName);
+}
